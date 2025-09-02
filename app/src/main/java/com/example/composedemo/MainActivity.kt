9d52f3cdec35e63316cf7fbec4bffd7351c19d60
SofiIlina <sofi0207@mail.ru>
2025-09-02 18:57:21 +0700

    Start new project "ComposeDemo"

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() { //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
